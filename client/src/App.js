1dabcb422dc492c3120d56e463f56303e97531c9
ashleyhodge <ashhodge@comcast.net>
2022-07-11 14:57:56 -0400

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Nav from './components/Nav';
+
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+      </div>
+    </Router>
   )
 }
 export default App;
