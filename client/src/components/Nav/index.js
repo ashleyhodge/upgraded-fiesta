662168871babc08d1da7198062c94a12dbd57b4a
ashleyhodge <ashhodge@comcast.net>
2022-07-11 16:22:27 -0400

    ass style to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,7 +7,7 @@ function Nav() {
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
         <a href="/" class="flex items-center">
             {/* <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" /> */}
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Stanley Jane</span>
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white drop-shadow-lg shadow-black">Stanley Jane</span>
         </a>
         <div class="flex items-center">
             <a href="tel:5541251234" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
@@ -20,16 +20,16 @@ function Nav() {
         <div class="flex items-center">
             <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
                 <li>
-                    <a href="#" class="text-indigo-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                    <a href="#" class="drop-shadow-xl shadow-black text-white dark:text-white hover:underline" aria-current="page">Home</a>
                 </li>
                 <li>
-                    <a href="#" class="text-indigo-900 dark:text-white hover:underline">Shop</a>
+                    <a href="#" class="drop-shadow-xl shadow-black text-white dark:text-white hover:underline">Shop</a>
                 </li>
                 <li>
-                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Blog</a>
+                    <a href="#" class="drop-shadow-xl shadow-black text-white dark:text-white hover:underline">Blog</a>
                 </li>
                 <li>
-                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Book Astrological Reading!</a>
+                    <a href="#" class="drop-shadow-xl shadow-black text-white dark:text-white hover:underline">Book Astrological Reading!</a>
                 </li>
             </ul>
         </div>
