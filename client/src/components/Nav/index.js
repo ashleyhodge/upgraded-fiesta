6be3fe4a8a2b624efd9c02b7e5a4a5ef8763966b
ashleyhodge <ashhodge@comcast.net>
2022-07-12 17:38:59 -0400

    add drop down menu to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,41 +1,64 @@
-import React from "react";
+
+import React, { useState } from "react";
+import { Navbar } from 'flowbite-react'
+import { AiOutlineMenu } from "react-icons/ai";
+
 
 function Nav() {
+  const [showNav, setShowNav] = useState(false);
   return (
-    <>
-    <nav class="bg-white border-purple-100 dark:bg-purple-100">
+<>
+    <nav class="bg-purple-200 border-purple-100 text-white">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
         <a href="/" class="flex items-center">
             {/* <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" /> */}
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white drop-shadow-lg shadow-black">Stanley Jane</span>
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white drop-shadow-lg shadow-black">Stanli Jane</span>
         </a>
         <div class="flex items-center">
-            <a href="tel:5541251234" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-            <a href="#" class="text-sm font-medium text-white dark:text-white hover:underline hover:text-midgreen-50">Login</a>
+          <a href="tel:5541251234" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
+          <a href="#" class="text-sm font-medium text-purple-50 dark:text-purple-50 hover:underline hover:text-white">Login</a>
+          {showNav ? (
+          <AiOutlineMenu
+            onClick={() => setShowNav(!showNav)}
+            className="md:hidden block w-10 h-10 p-2 cursor-pointer"
+          />
+        ) : (
+          <AiOutlineMenu
+            onClick={() => setShowNav(!showNav)}
+            className="md:hidden block w-10 h-10 p-2 cursor-pointer"
+          />
+        )}
         </div>
     </div>
 </nav>
-<nav class="bg-purple-50 dark:bg-purple-50">
-    <div class="py-3 px-4 mx-auto max-w-screen-xl md:px-6">
-        <div class="flex items-center">
-            <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+<nav class="bg-purple-100">
+    <div class="py-3 px-4 mx-auto max-w-screen-xl">
+    
+        <div class="flex items-center w-full md:block ">
+            <ul className={
+          (showNav ? "left-0" : "-left-full") +
+          " md:static fixed bottom-15 top-12 md:flex md:space-x-7 items-center md:bg-transparent bg-purple-100  md:w-full w-full md:text-purple-100 text-white md:space-y-0 space-y-5 p-2 transition-left"
+        }>
                 <li>
-                    <a href="#" class="drop-shadow-xl shadow-black text-white dark:text-white hover:underline hover:text-midgreen-50" aria-current="page">Home</a>
+                    <a href="#" class="drop-shadow-lg shadow-black text-purple-50 dark:text-purple-50 hover:underline hover:text-white" aria-current="page">Home</a>
                 </li>
                 <li>
-                    <a href="#" class="drop-shadow-xl shadow-black text-white dark:text-white hover:underline hover:text-midgreen-50">Shop</a>
+                    <a href="#" class="drop-shadow-lg shadow-black text-purple-50 dark:text-purple-50 hover:underline hover:text-white">Shop</a>
                 </li>
                 <li>
-                    <a href="#" class="drop-shadow-xl shadow-black text-white dark:text-white hover:underline hover:text-midgreen-50">Blog</a>
+                    <a href="#" class="drop-shadow-lg shadow-black text-purple-50 dark:text-purple-50 hover:underline hover:text-white">Blog</a>
                 </li>
                 <li>
-                    <a href="#" class="drop-shadow-xl shadow-black text-white dark:text-white hover:underline hover:text-midgreen-50">Book Astrological Reading!</a>
+                    <a href="#" class="drop-shadow-lg shadow-black text-purple-50 dark:text-purple-50 hover:underline hover:text-white">Book Astrological Reading!</a>
                 </li>
             </ul>
         </div>
     </div>
-</nav>
+  </nav>
 </>
-  )
+  );
 }
+
+
 export default Nav;
+
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,23 +2,20 @@
 module.exports = {
   content: [
     "./src/**/*.{js, jsx, ts, tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
       colors: {
         purple: {
           50: '#dbddea',
-          100: '#6974C9',
-          200: '#36213E'
-        },
-        midgreen: {
-          50: '#0F5257'
-        },
-        yellow: {
-          50: '#9BCD7A'
+          100: '#7278AC',
+          200: '#444A74'
         }
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
