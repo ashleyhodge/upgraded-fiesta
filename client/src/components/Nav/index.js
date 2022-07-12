a9c1966e946004ae4f17a0440d364f59f2d7bc23
ashleyhodge <ashhodge@comcast.net>
2022-07-12 17:46:49 -0400

    minor change

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import { Navbar } from 'flowbite-react'
+
 import { AiOutlineMenu } from "react-icons/ai";
 
 
@@ -9,7 +9,7 @@ function Nav() {
   return (
 <>
     <nav class="bg-purple-200 border-purple-100 text-white">
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 sm:px-6 py-2.5">
         <a href="/" class="flex items-center">
             {/* <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" /> */}
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white drop-shadow-lg shadow-black">Stanli Jane</span>
@@ -20,24 +20,24 @@ function Nav() {
           {showNav ? (
           <AiOutlineMenu
             onClick={() => setShowNav(!showNav)}
-            className="md:hidden block w-10 h-10 p-2 cursor-pointer"
+            className="sm:hidden block w-10 h-10 p-2 cursor-pointer"
           />
         ) : (
           <AiOutlineMenu
             onClick={() => setShowNav(!showNav)}
-            className="md:hidden block w-10 h-10 p-2 cursor-pointer"
+            className="sm:hidden block w-10 h-10 p-2 cursor-pointer"
           />
         )}
         </div>
     </div>
 </nav>
 <nav class="bg-purple-100">
-    <div class="py-3 px-4 mx-auto max-w-screen-xl">
+    <div class="px-4 mx-auto max-w-screen-xl sm:px-6">
     
-        <div class="flex items-center w-full md:block ">
+        <div class="flex items-center w-full sm:block ">
             <ul className={
           (showNav ? "left-0" : "-left-full") +
-          " md:static fixed bottom-15 top-12 md:flex md:space-x-7 items-center md:bg-transparent bg-purple-100  md:w-full w-full md:text-purple-100 text-white md:space-y-0 space-y-5 p-2 transition-left"
+          " sm:static fixed bottom-15 top-12 sm:flex sm:space-x-7 items-center sm:bg-transparent bg-purple-100  sm:w-full w-full sm:text-purple-100 text-white sm:space-y-0 space-y-5 p-2 transition-left"
         }>
                 <li>
                     <a href="#" class="drop-shadow-lg shadow-black text-purple-50 dark:text-purple-50 hover:underline hover:text-white" aria-current="page">Home</a>
