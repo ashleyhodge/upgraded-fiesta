85ac78f32c9ec1c31b6855e4801f761d7bb87db1
ashleyhodge <ashhodge@comcast.net>
2022-07-18 12:16:30 -0400

    add product queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,10 +2,13 @@ const { Product } = require('../models');
 
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello World!';
+    product: async (parent, { _id }) => {
+      return await Product.findById(_id);
+    },
+    products: async () => {
+      return Product.find()
     }
-  }
-};
+    }
+  };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,18 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+  }
+
   type Query {
-    helloWorld: String
+    products: [Product]
+    product(_id: ID!): Product
   }
 `;
 
