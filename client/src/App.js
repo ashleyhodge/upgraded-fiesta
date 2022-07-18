4d0451a19e6042cfd3ec706182be854dd144107a
ashleyhodge <ashhodge@comcast.net>
2022-07-18 18:06:13 -0400

    render products in store

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,23 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+
 
 import Nav from './components/Nav';
 import Store from './pages/Store';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache: new InMemoryCache(),
 });
+
+
 
 function App() {
   return (
--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_ALL_PRODUCTS } from '../utils/queries';
 import ProductList from '../components/productList';
 
 const Store = () => {
+    // use useQuery hook to make query request
+  const {loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+  const products = data?.products || [];
+  console.log(products)
+  
   return (
-    <div className='container'>
-      <ProductList />
+<main>
+  <div className="flex-row justify-space-between">
+    <div className="col-12 mb-3">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ProductList products={products}  />
+      )}
     </div>
+  </div>
+</main>
   )
 }
 
